343bacde2342b5adb2629d358987e9395d09954a
mitchell amihod <4623+meeech@users.noreply.github.com>
2024-02-27 18:47:59 -0500

    it works

--- Spoons/PickAndStick.spoon/init.lua
+++ Spoons/PickAndStick.spoon/init.lua
@@ -9,9 +9,6 @@ obj.author = "meeech <meeeech@omg.lol>"
 obj.homepage = "https://meeech.omg.lol"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
--- Logger
-obj.logger = hs.logger.new('PickAndStick')
-
 --- PickAndStick.logger
 --- Variable
 --- Logger object used within the Spoon. Can be accessed to set the default log level for the messages coming from the Spoon.
@@ -26,6 +23,13 @@ obj.show_in_menubar = true
 --- Variable
 --- Title to show in the menubar if `show_in_menubar` is true. [Emojis](http://emojipedia.org/rainbow/)
 obj.menubar_title = "\u{1F4CC}"
+
+-- PIckAndStick.preferred_inputs
+-- Variable
+-- List of user preferred inputs, in order of precedence
+obj.preferred_inputs = {}
+
+local deviceInputChangedCode = 'dIn '
 
 -- Return the sorted keys of a table
 function sortedkeys(tab)
@@ -46,20 +50,57 @@ end
 --     return tab
 -- end
 
-function showMenu()
-    obj.logger.w("Show menu")
-
+local function listInputs()
     -- get list of audio inputs
     local inputs = hs.audiodevice.allInputDevices()
     local keys = sortedkeys(inputs)
     local tab = {}
 
-    for i, v in ipairs(keys) do
-        obj.logger.w(i, v, inputs[v]:name())
-        -- table.insert(tab, { title = v, fn = hs.fnutils.partial(obj.toggleColorSamples, v) })
+    for _, v in ipairs(keys) do
+        obj.logger.w("Input key: %s: %s", v, inputs[v]:name())
+    end
+end
+
+-- https://www.hammerspoon.org/docs/hs.audiodevice.watcher.html#setCallback
+function PSonInputChange(whatChanged)
+    if whatChanged ~= deviceInputChangedCode then
+        obj.logger.i('Input not change')
+        return
     end
 
-    -- obj.choosermenu:popupMenu(hs.mouse.getAbsolutePosition())
+    for _, v in ipairs(obj.preferred_inputs) do
+        local inputDevice = hs.audiodevice.findInputByName(v)
+        obj.logger.w('Input device', inputDevice)
+        if inputDevice ~= nil then
+            local success = inputDevice:setDefaultInputDevice()
+            if success == false then
+                obj.logger.w('There was a problem setting input to:', v)
+            else
+                obj.logger.w('Set input to:', v)
+            end
+            return
+        end
+    end
+end
+
+-- preferred_inputs list of preferred inputs
+function obj:start(preferred_inputs)
+    if preferred_inputs == nil or next(preferred_inputs) == nil then
+        obj.logger.w("No preferred inputs specified. Nothing to do.")
+        return
+    end
+    self.preferred_inputs = preferred_inputs
+
+    PSonInputChange(deviceInputChangedCode)
+
+    hs.audiodevice.watcher.setCallback(PSonInputChange)
+    hs.audiodevice.watcher:start()
+end
+
+function obj:stop()
+    -- stop audio input change watcher
+    hs.audiodevice.watcher:start()
+    hs.audiodevice.watcher.setCallback(nil)
 end
 
 function obj:init()
@@ -68,34 +109,9 @@ function obj:init()
         self.choosermenu = hs.menubar.new(true, 'picknstick')
         self.choosermenu:setTitle(self.menubar_title)
         self.choosermenu:setClickCallback(function()
-            obj.logger.w("No action on click yet.")
+            listInputs()
         end)
     end
-end
-
-local function onInputChange()
-
-end
-
--- preferred_inputs list of preferred inputs
-function obj:start(preferred_inputs)
-    -- if preferred_inputs is empty, give error message
-    -- check in lua if the preferred_inputs is empty
-
-    if preferred_inputs == nil or next(preferred_inputs) == nil then
-        obj.logger.w("No preferred inputs specified. Nothing to do.")
-        return
-    end
-
-    -- call initial Set Audio Input
-    -- set up watcher
-    -- local inputs = hs.audiodevice.allInputDevices()
-
-    -- start audio input change watcher
-end
-
-function obj:stop()
-    -- stop audio input change watcher
 end
 
 return obj
